b73a8d43889b74d30160ee2f957af1548b617597
flexfinRTP <gaffney311@outlook.com>
2023-09-20 07:25:44 -0500

    deploy fix types

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
     webpack: config => {
       config.resolve.fallback = { fs: false, net: false, tls: false };
       return config;
-    },
-    eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
     },
   };
   
